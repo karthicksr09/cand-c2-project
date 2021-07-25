6ef1a4c55367978f4112fa384c7b5f2821359cc7
karthicksr09 <59381877+karthicksr09@users.noreply.github.com>
2021-07-25 11:47:32 -0500

    Add files via upload

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,39 @@
-# TODO: Designate a cloud provider, region, and credentials
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 
+  required_version = ">= 0.14.9"
+}
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
 
+resource "aws_instance" "Udacity_T2" {
+  ami                    = "ami-0dc2d3e4c0f9ebd18"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-06297cfca142d08ed"]
+  subnet_id              = "subnet-0783d43c6da8a89d9"
+  count                  = 4
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+  tags = {
+    Name = "Udacity_T2"
+  }
+}
+
+resource "aws_instance" "Udacity_M4" {
+  ami                    = "ami-0dc2d3e4c0f9ebd18"
+  instance_type          = "m4.large"
+  vpc_security_group_ids = ["sg-06297cfca142d08ed"]
+  subnet_id              = "subnet-0783d43c6da8a89d9"
+  count                  = 2
+
+  tags = {
+    Name = "Udacity_M4"
+  }
+}
